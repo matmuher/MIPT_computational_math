95a5e3ef35202aafb6575fe349562bad625550a0
matmuher <barbusas.mail@gmail.com>
2023-10-11 16:13:47 +0300

    [2week] Change task matrix

--- 2_system_of_linear_equations/main.py
+++ 2_system_of_linear_equations/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from abc import ABC, abstractmethod
 
 '''
 
@@ -26,24 +26,44 @@ class LinearEqSystemSolver:
 
 '''
 
+class LinearEqSystemSolver(ABC):
 
-# Create matrix from the task: г (гэ)
+	@abstractmethod
+	def solve(self, A, b):
+		pass
+
+# class GaussianSovler(LinearEqSystemSolver):
+
+# 	def solve(slef, A, b):
+
+
+
+# Create matrix from the task: y
 
 def construct_task_matrix(N):
 
-	A = np.triu(np.ones((N, N),  dtype = np.float64), -1)
-	A = A + np.identity(N, dtype = np.float64) * 9
+	A = np.identity(N, dtype = np.float64)
 
-	A[0][0] = A[0][0] - 9
-	A[N-1][N-1] = A[N-1][N-1] - 9
+	for i in range(1, N+1):
 
-	b = np.arange(100, 0, -1, dtype = np.float64)
+		for j in range(1, N+1):
+
+			if i != j:
+				
+				A[i-1][j-1] = 1 / (i*i + j) 
+
+	b = np.arange(1, N+1, dtype = np.float64)
+	
+	for i in range(1, N+1):
+
+		b[i-1] = 1/(i)
 
 	return A, b
 
 if __name__ == '__main__':
 
-	A, b = construct_task_matrix(100)
+	A, b = construct_task_matrix(12)
 
 	print(A)
 	print(b)
+
