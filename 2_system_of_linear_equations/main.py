8f9cb32c8fbeb0ad506836a69e2898744dac43d7
matmuher <barbusas.mail@gmail.com>
2023-10-11 17:08:02 +0300

    [2week: Gauss] Transform matrix to triangle

--- 2_system_of_linear_equations/main.py
+++ 2_system_of_linear_equations/main.py
@@ -32,15 +32,55 @@ class LinearEqSystemSolver(ABC):
 	def solve(self, A, b):
 		pass
 
-# class GaussianSovler(LinearEqSystemSolver):
+'''
 
-# 	def solve(slef, A, b):
+1. Exclude x1 from down equations. By adding 1st equation muliplied by -a11/ai1.
+2. Then exlide x2 in the same way.
+...
+3. We get triangle matrix. Answer can be got by walking from down to up.
 
+'''
 
+class GaussianSolver(LinearEqSystemSolver):
+
+	def solve(self, A, b):
+
+		equs = np.column_stack((A, b))
+
+		N = b.size
+
+		for exluder_equ_index in range(0, N-1):
+
+			exluder_equ = equs[exluder_equ_index]
+
+			for current_equ_index in range(exluder_equ_index + 1,  N):
+
+				current_equ = equs[current_equ_index] 
+
+				if current_equ[exluder_equ_index] == 0:
+
+					continue
+
+				mutiplier = - current_equ[exluder_equ_index] / exluder_equ[exluder_equ_index]
+				current_equ += exluder_equ * mutiplier
+
+		print_matrix(equs)
+
+def print_matrix(matrix):
+
+	for i in range(0, np.shape(matrix)[0]):
+
+		for j in range(0, np.shape(matrix)[1]):
+
+			print(f'\t{matrix[i][j]:.2}', end = ' ')
+
+		print('\n')
 
 # Create matrix from the task: y
 
-def construct_task_matrix(N):
+def construct_task_matrix():
+
+	N = 12
 
 	A = np.identity(N, dtype = np.float64)
 
@@ -62,8 +102,7 @@ def construct_task_matrix(N):
 
 if __name__ == '__main__':
 
-	A, b = construct_task_matrix(12)
+	A, b = construct_task_matrix()
 
-	print(A)
-	print(b)
-
+	solver = GaussianSolver()
+	solver.solve(A, b)
